e92f9f505ee18932fead989332b43daac2ac5765
eldar <e.mingazov001@gmail.com>
2020-04-17 00:33:08 +0600

    add vuetify to project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <div id="app">
-    <Auth/>
-  </div>
+  <v-app>
+    <v-content>
+      <Auth/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Auth from './components/Auth'
+import Auth from "@/components/Auth";
+
 export default {
   name: 'App',
-  components: {
-    Auth
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Auth,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>
-        <form @submit.prevent="submit">
-            <input type="text" placeholder="username" v-model="username"/>
-            <input type="text" placeholder="password" v-model="password"/>
-            <button type="submit">GO</button>
-        </form>
-        <hr>
-        <h3>{{getAuthToken}}</h3>
-    </div>
+    <v-app id="inspire">
+        <v-content>
+            <v-container class="fill-height" fluid>
+                <v-row align="center" justify="center">
+                    <v-col cols="12" sm="8" md="4">
+                        <v-card class="elevation-12">
+                            <v-toolbar color="primary" dark flat>
+                                <v-spacer />
+                                <v-toolbar-title>Mercy Group</v-toolbar-title>
+                                <v-spacer />
+                            </v-toolbar>
+                            <v-card-text>
+                                <v-form @submit.prevent="submit" id="signIn">
+                                    <v-text-field label="Login" name="login" v-model="username" prepend-icon="person" type="text"/>
+                                    <v-text-field id="password" label="Password" v-model="password" name="password" prepend-icon="lock" type="password"/>
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer/>
+                                <v-btn type="submit" color="primary" form="signIn">Sign In</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
