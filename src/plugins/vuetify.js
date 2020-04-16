0b5b4d12ce855cd7493292e238b0b8e9957b451e
eldar <e.mingazov001@gmail.com>
2020-04-17 00:30:25 +0600

    Добавил link  для отображения иконок vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#2196f3',
+                secondary: '#00bcd4',
+                accent: '#ffc107',
+                error: '#f44336',
+                warning: '#ff5722',
+                info: '#607d8b',
+                success: '#4caf50'
+            },
+        },
+    },
+});
